3f0f191de782511ea3ea9c6aa0adbc8a2407d6d6
ShakirAhmed <ashakir3317@gmail.com>
2026-06-12 20:55:48 -0400

    completed lesson practice

---
+++ public_html/m02/lesson/def_vars.php
@@ -0,0 +1,8 @@
+<?php
+$course_code = "IT202";
+$attempt_count = 1;
+$is_ready = true;
+
+echo $course_code;
+var_dump($attempt_count);
+var_export($is_ready);
---
+++ public_html/m02/lesson/operators.php
@@ -0,0 +1,7 @@
+<?php
+$attemptText = "2";
+$attemptNumber = 2;
+
+echo "Attempt " . $attemptNumber;
+var_dump($attemptText == $attemptNumber);
+var_dump($attemptText === $attemptNumber);
