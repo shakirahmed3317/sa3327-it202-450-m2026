4d398ba03dd81d7a7b329794413b9e6a88cac21b
ShakirAhmed <ashakir3317@gmail.com>
2026-06-15 17:33:07 -0400

    Finished lesson 2 work

---
+++ public_html/m02/lesson/type_check.php
@@ -0,0 +1,11 @@
+<?php
+$attemptText = "2";
+$attemptNumber = 2;
+
+echo "Text value: ";
+// var_dump shows both the type and the value.
+var_dump($attemptText);
+
+echo "Number value: ";
+var_dump($attemptNumber);
+?>
---
+++ public_html/m02/lesson/while.php
@@ -0,0 +1,12 @@
+<?php
+$i = 0;
+while ($i < 3) {
+    echo "while: $i<br>";
+    $i++;
+}
+
+$j = 0;
+do {
+    echo "do while: $j<br>";
+    $j++;
+} while ($j < 3);
